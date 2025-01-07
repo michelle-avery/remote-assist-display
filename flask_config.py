64e31c4760ddfefe892925038ccc1cbc33e80268
Michelle Avery <michelleavery8@gmail.com>
2025-01-07 12:15:05 -0500

    decouple assistant and remote assist display by listening specifically to navigate events

--- flask_config.py
+++ flask_config.py
@@ -10,7 +10,7 @@ class Config(object):
     # Number of times to retry fetching token
     TOKEN_RETRY_LIMIT = env.int("TOKEN_RETRY_LIMIT", 10)
     # Event type to listen for
-    EVENT_TYPE = env.str("EVENT_TYPE", "custom_conversation_conversation_ended")
+    EVENT_TYPE = env.str("EVENT_TYPE", "remote_assist_display_navigate")
     # The local storage key to use for the device name. By default, this is the
     # key used by browser_mod, to  maintain ViewAssist compatibility
     DEVICE_NAME_KEY = env.str("DEVICE_NAME_KEY", "browser_mod-browser-id")
