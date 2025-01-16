59841af45d18c49d4122444111a5e61886a3140e
Michelle Avery <michelleavery8@gmail.com>
2025-01-16 13:39:05 -0500

    Make window frameless and full screen

--- application/main.py
+++ application/main.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     stream = StringIO()
     with redirect_stdout(stream):
         app = create_app()
-        window = webview.create_window('Remote Assist Display', app)
+        # window is created fullscreen, unless running in debug mode
+        window = webview.create_window('Remote Assist Display', app, frameless=True, fullscreen=not app.debug)
         webview.start(debug=app.debug, private_mode=False)
