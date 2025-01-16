90021cc55b9301e22a8fc1b6724738258cda3aa8
Michelle Avery <michelleavery8@gmail.com>
2025-01-16 09:20:38 -0500

    remove event listening stuff for now

--- custom_components/remote_assist_display/sensor.py
+++ custom_components/remote_assist_display/sensor.py
@@ -75,38 +75,3 @@ class RADSensor(RADEntity, SensorEntity):
     def extra_state_attributes(self):
         """Return the state attributes."""
         return super().extra_state_attributes
-
-
-class RADIntentSensor(SensorEntity):
-    """Entity to represent the intent of the most recent conversation event received for this device."""
-
-    entity_description = SensorEntityDescription(
-        key="intent",
-        icon="mdi:comment-question",
-    )
-
-    def __init__(self, config_entry) -> None:
-        """Initialize the sensor."""
-        self._config_entry = config_entry
-        if config_entry.unique_id:
-            self._attr_unique_id = (
-                f"{config_entry.unique_id}-{self.entity_description.key}"
-            )
-        else:
-            self._attr_unique_id = f"{config_entry.title}-{self.entity_description.key}"
-        self._attr_native_value = None
-        self._attr_extra_state_attributes = {}
-        self._state = None
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, config_entry.unique_id)}
-        )
-
-    @callback
-    def update_from_event(self, result: dict[str, Any]) -> None:
-        """Update the sensor state from the event data."""
-        self._attr_extra_state_attributes = {"intent_output": result}
-        if "speech" in result["response"] and "plain" in result["response"]["speech"]:
-            self._attr_native_value = result["response"]["speech"]["plain"].get(
-                "speech", ""
-            )
-        self.async_write_ha_state()
