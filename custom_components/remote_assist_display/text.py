509ae80927c55dccaa8deec6df7bd0ce2dd80db5
Michelle Avery <michelleavery8@gmail.com>
2025-01-23 14:12:18 -0500

    Get default dashboard setting at the integration level

--- custom_components/remote_assist_display/text.py
+++ custom_components/remote_assist_display/text.py
@@ -1,9 +1,16 @@
+from typing import Any
+
 from homeassistant.components.text import RestoreText
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from typing import Any
+
+from .const import (
+    DATA_ADDERS,
+    DOMAIN,
+    DEFAULT_DISPLAY_OPTIONS,
+    DEFAULT_HOME_ASSISTANT_DASHBOARD,
+)
 from .entities import RADEntity
-from .const import DOMAIN, DATA_ADDERS, DATA_STORE
 
 
 async def async_setup_platform(
@@ -14,6 +21,7 @@ async def async_setup_platform(
 ) -> None:
     """Set up the text platform."""
     hass.data[DOMAIN][DATA_ADDERS]["text"] = async_add_entities
+    hass.data[DOMAIN][DEFAULT_DISPLAY_OPTIONS] = dict(config_entry.options)
 
 
 async def async_setup_entry(
@@ -22,7 +30,7 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up text entities."""
-    await async_setup_platform(hass, {}, async_add_entities)
+    await async_setup_platform(hass, config_entry, async_add_entities)
 
 
 class DefaultDashboardText(RADEntity, RestoreText):
@@ -43,39 +51,30 @@ class DefaultDashboardText(RADEntity, RestoreText):
         if (last_text_data := await self.async_get_last_text_data()) is None:
             self._attr_native_value = None
             return
-        # If the entity state was last unknown, the native value here will still be None,
-        # so restore it from settings.
-        if not last_text_data.native_value:
-            self._attr_native_value = (
-                self.hass.data[DOMAIN][DATA_STORE]
-                .get_display(self.display_id)
-                .asdict()
-                .get("settings", {})
-                .get("default_dashboard", None)
-            )
         else:
             self._attr_native_value = last_text_data.native_value
 
     @property
     def native_value(self):
-        val = self._data.get("display", {}).get("default_dashboard", None)
+        val = self._data.get("settings", {}).get("default_dashboard", None)
         if not val:
-            val = self._data.get("settings", {}).get("default_dashboard", None)
-        if not val:
-            val = self._attr_native_value
+            try:
+                val = self._attr_native_value
+            except AttributeError:
+                val = self.coordinator.hass.data[DOMAIN][DEFAULT_DISPLAY_OPTIONS].get(
+                    "default_dashboard_path", DEFAULT_HOME_ASSISTANT_DASHBOARD
+                )
         if len(str(val)) > 255:
             val = str(val)[:250] + "..."
         return val
 
     async def async_set_value(self, value: str) -> None:
-        """Set the default dashboard both in the entities value and in the settings store."""
+        """Set the default dashboard."""
         self._value = value
         data = {
             "settings": {"default_dashboard": value},
             "display": {"default_dashboard": value},
         }
-        # self.display.update_settings(self.hass, data)
-        store = self.hass.data[DOMAIN][DATA_STORE]
-        await store.async_set_display(self.display.display_id, **data)
+        self.display.update_settings(self.hass, data)
         self._data.update(data)
         self.async_write_ha_state()
