5f0b2398b67e1162d39c79d4406adcfaae14f4fe
Michelle Avery <michelleavery8@gmail.com>
2025-01-24 14:47:45 -0500

    Enhance debug settings and logging. There are now four separate config settings: LOG_LEVEL defaults to INFO and controls log settings FLASK_DEBUG controls flask's debug behavior FULLSCREEN determines whether the window launches full screen or not WEBVIEW_DEBUG launches the browser developer tools window for the pywebview window

--- application/main.py
+++ application/main.py
@@ -1,15 +1,13 @@
 from contextlib import redirect_stdout
 from io import StringIO
 
-from remote_assist_display import create_app
-
 import webview
 
+from remote_assist_display import create_app
 
 if __name__ == '__main__':
     stream = StringIO()
     with redirect_stdout(stream):
         app = create_app()
-        # window is created fullscreen, unless running in debug mode
-        window = webview.create_window('Remote Assist Display', app, frameless=True, fullscreen=not app.debug)
-        webview.start(debug=app.debug, private_mode=False)
+        window = webview.create_window('Remote Assist Display', app, frameless=True, fullscreen=app.config['FULLSCREEN'])
+        webview.start(debug=app.config['WEBVIEW_DEBUG'], private_mode=False)
--- application/remote_assist_display/__init__.py
+++ application/remote_assist_display/__init__.py
@@ -1,4 +1,6 @@
+import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 from flask import Flask
 
@@ -8,6 +10,34 @@ from .flask_config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.debug = app.config['FLASK_DEBUG']
+
+    log_level = getattr(logging, app.config['LOG_LEVEL'])
+    
+    logs_dir = os.path.join(os.path.dirname(__file__), '..', 'logs')
+    os.makedirs(logs_dir, exist_ok=True)
+    
+    file_handler = RotatingFileHandler(
+        os.path.join(logs_dir, 'remote_assist_display.log'),
+        maxBytes=1024 * 1024,  # 1MB
+        backupCount=10
+    )
+    file_handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    ))
+    file_handler.setLevel(log_level)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    ))
+    console_handler.setLevel(log_level)
+
+    app.logger.setLevel(log_level)
+    app.logger.addHandler(file_handler)
+    app.logger.addHandler(console_handler)
+    
+    app.logger.handlers = [file_handler, console_handler]
     gui_dir = os.path.join(os.path.dirname(__file__), "templates")  # development path
     if not os.path.exists(gui_dir):  # frozen executable path
         gui_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
