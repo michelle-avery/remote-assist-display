57586df9b6b9f15aaddffe9baee98877d8afe3fb
Michelle Avery <michelleavery8@gmail.com>
2025-01-27 13:43:46 -0500

    Add stroage key text class

--- custom_components/remote_assist_display/text.py
+++ custom_components/remote_assist_display/text.py
@@ -9,6 +9,7 @@ from .const import (
     DOMAIN,
     DEFAULT_DISPLAY_OPTIONS,
     DEFAULT_HOME_ASSISTANT_DASHBOARD,
+    DEFAULT_DEVICE_NAME_STORAGE_KEY,
 )
 from .entities import RADEntity
 
@@ -74,6 +75,50 @@ class DefaultDashboardText(RADEntity, RestoreText):
         data = {
             "settings": {"default_dashboard": value},
             "display": {"default_dashboard": value},
+        }
+        self.display.update_settings(self.hass, data)
+        self._data.update(data)
+        self.async_write_ha_state()
+
+class DeviceStorageKeyText(RADEntity, RestoreText):
+    def __init__(
+        self,
+        coordinator,
+        display_id,
+        display,
+    ):
+        """Initialize the text."""
+        RADEntity.__init__(self, coordinator, display_id, "Device Storage Key")
+        RestoreText.__init__(self)
+        self.display = display
+
+    async def async_added_to_hass(self):
+        """Run when entity about to be added to hass."""
+        await super().async_added_to_hass()
+        if (last_text_data := await self.async_get_last_text_data()) is None:
+            self._attr_native_value = None
+            return
+        else:
+            self._attr_native_value = last_text_data.native_value
+
+    @property
+    def native_value(self):
+        val = self._data.get("settings", {}).get("device_name_storage_key", None)
+        if not val:
+            try:
+                val = self._attr_native_value
+            except AttributeError:
+                val = self.coordinator.hass.data[DOMAIN][DEFAULT_DISPLAY_OPTIONS].get(
+                    "device_name_storage_key", DEFAULT_DEVICE_NAME_STORAGE_KEY
+                )
+        return val
+
+    async def async_set_value(self, value: str) -> None:
+        """Set the device storage key."""
+        self._value = value
+        data = {
+            "settings": {"device_name_storage_key": value},
+            "display": {"device_name_storage_key": value},
         }
         self.display.update_settings(self.hass, data)
         self._data.update(data)
