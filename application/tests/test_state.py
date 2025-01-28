b6dead55c12eddb7c9ceb605f7c5f5030fc4a935
Michelle Avery <michelleavery8@gmail.com>
2025-01-28 10:21:13 -0500

    add a test for that

--- application/tests/test_state.py
+++ application/tests/test_state.py
@@ -26,6 +26,17 @@ async def test_local_storage_set_on_url_load(app, mock_state_webview):
     )
 
 @pytest.mark.asyncio
+async def test_local_storage_not_set_on_url_load(app, mock_state_webview):
+    """Test that localStorage is not set when loading a URL."""
+    mock_webview, mock_window = mock_state_webview
+    app.config["DEVICE_NAME_KEY"] = "device_name"
+    state = DisplayState.get_instance()
+    
+    await state.load_url("http://test.url", local_storage=False)
+    
+    mock_window.evaluate_js.assert_not_called()
+
+@pytest.mark.asyncio
 async def test_local_storage_set_on_card_load(app, mock_state_webview):
     """Test that localStorage is set when loading a card."""
     mock_webview, mock_window = mock_state_webview
