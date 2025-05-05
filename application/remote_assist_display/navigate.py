617d8fa919c306cbb8fa3babff1b06093ff30e41
Michelle Avery <michelleavery8@gmail.com>
2025-05-05 10:52:40 -0400

    Remove deprecated pywebview-based application

--- application/remote_assist_display/navigate.py
+++
@@ -1,9 +0,0 @@
-from .state import DisplayState
-
-
-async def load_card(event, expire_time=None):
-    await DisplayState.get_instance().load_card(event, expire_time)
-
-
-async def load_dashboard(url, local_storage=True):
-    await DisplayState.get_instance().load_url(url, local_storage)
